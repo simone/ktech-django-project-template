01e73c1c7439f3f158b090433a60a0fc2da2f1f9
Simone Federici <s.federici@gmail.com>
2012-02-13 23:28:46 +0100

    deleted old location doc and update setup for upload and register

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
-from kdist import setup, cmdclasses, configure, parse_requirements, Nope
+from kdist import setup, cmdclasses, configure, parse_requirements
 
 packages, data_files, package_dir = configure('src/python', '{{ project_name }}')
 version = __import__('{{ project_name }}').get_version()
-
-# deactivate register and upload on pypi
-cmdclasses.update({'register': Nope, 'upload':Nope})
 
 # parse requirements.txt
 requirements, dependency_links = parse_requirements('requirements.txt')
